5412f0459b5934c2ac6102e4e477a9aec6e1f19c
Felipe C. de Pauli <fcdpauli@gmail.com>
2023-05-11 17:49:06 -0300

    ud_yolo_03: add the detection using a video as input

--- UY_03_Train_YOLO_for_Object_Detection/02_Class/02_Yolo_on_Video.py
+++ UY_03_Train_YOLO_for_Object_Detection/02_Class/02_Yolo_on_Video.py
@@ -1,215 +1,91 @@
 
 
-"""
-Course:  Training YOLO v3 for Objects Detection with Custom Data
+# 1. Reading frames from a video in loop
+# 2. Getting blob from the frame
+# 3. Implementing Forward Pass
+# 4. Getting Bounding Box co-ordinates, confidence score, class-id from the output
+# 5. Non-maximum Suppression
+# 6. Drawing bounding box with class label on the image
+# 7. Writing processed image back to a video
 
-Section-2
-Objects Detection on Video with YOLO v3 and OpenCV
-File: yolo-3-video.py
-"""
+# Result: New video file with detected objects, boungin boxes and labels
 
+# Imports
 
-# Detecting Objects on Video with OpenCV deep learning library
-#
-# Algorithm:
-# Reading input video --> Loading YOLO v3 Network -->
-# --> Reading frames in the loop --> Getting blob from the frame -->
-# --> Implementing Forward Pass --> Getting Bounding Boxes -->
-# --> Non-maximum Suppression --> Drawing Bounding Boxes with Labels -->
-# --> Writing processed frames
-#
-# Result:
-# New video file with Detected Objects, Bounding Boxes and Labels
-
-
-# Importing needed libraries
-import numpy as np
 import cv2
+import numpy as np
 import time
 
-
-"""
-Start of:
-Reading input video
-"""
-
-# Defining 'VideoCapture' object
-# and reading video from a file
-# Pay attention! If you're using Windows, the path might looks like:
-# r'videos\traffic-cars.mp4'
-# or:
-# 'videos\\traffic-cars.mp4'
-video = cv2.VideoCapture('videos/traffic-cars.mp4')
-
-# Preparing variable for writer
-# that we will use to write processed frames
+video = cv2.VideoCapture("./videos/traffic-cars.mp4")
 writer = None
-
 # Preparing variables for spatial dimensions of the frames
 h, w = None, None
 
-"""
-End of:
-Reading input video
-"""
+# Loading Yolo v3 network
 
-
-"""
-Start of:
-Loading YOLO v3 network
-"""
-
-# Loading COCO class labels from file
-# Opening file
-# Pay attention! If you're using Windows, yours path might looks like:
-# r'yolo-coco-data\coco.names'
-# or:
-# 'yolo-coco-data\\coco.names'
 with open('yolo-coco-data/coco.names') as f:
-    # Getting labels reading every line
-    # and putting them into the list
     labels = [line.strip() for line in f]
 
+# Check point
+print('List with labels names:')
+print(labels)
 
-# # Check point
-# print('List with labels names:')
-# print(labels)
-
-# Loading trained YOLO v3 Objects Detector
-# with the help of 'dnn' library from OpenCV
-# Pay attention! If you're using Windows, yours paths might look like:
-# r'yolo-coco-data\yolov3.cfg'
-# r'yolo-coco-data\yolov3.weights'
-# or:
-# 'yolo-coco-data\\yolov3.cfg'
-# 'yolo-coco-data\\yolov3.weights'
-network = cv2.dnn.readNetFromDarknet('yolo-coco-data/yolov3.cfg',
-                                     'yolo-coco-data/yolov3.weights')
+network = cv2.dnn.readNetFromDarknet("./yolo-coco-data/yolov3.cfg", "./yolo-coco-data/yolov3.weights")
 
 # Getting list with names of all layers from YOLO v3 network
 layers_names_all = network.getLayerNames()
 
-# # Check point
-# print()
-# print(layers_names_all)
-
-# Getting only output layers' names that we need from YOLO v3 algorithm
-# with function that returns indexes of layers with unconnected outputs
 layers_names_output = [\
     layers_names_all[i - 1] for i in network.getUnconnectedOutLayers()]
 
-# # Check point
-# print()
-# print(layers_names_output)  # ['yolo_82', 'yolo_94', 'yolo_106']
-
 # Setting minimum probability to eliminate weak predictions
 probability_minimum = 0.5
-
-# Setting threshold for filtering weak bounding boxes
-# with non-maximum suppression
+# Setting minimum threshold for non-maximum suppression
 threshold = 0.3
 
-# Generating colours for representing every detected object
-# with function randint(low, high=None, size=None, dtype='l')
+# Generating randomly colours for representing every detected object
 colours = np.random.randint(0, 255, size=(len(labels), 3), dtype='uint8')
 
-# # Check point
-# print()
-# print(type(colours))  # <class 'numpy.ndarray'>
-# print(colours.shape)  # (80, 3)
-# print(colours[0])  # [172  10 127]
-
-"""
-End of:
-Loading YOLO v3 network
-"""
-
-
-"""
-Start of:
-Reading frames in the loop
-"""
-
-# Defining variable for counting frames
-# At the end we will show total amount of processed frames
-f = 0
-
-# Defining variable for counting total time
-# At the end we will show time spent for processing all frames
-t = 0
+f = 0   # Definig variable for counting frames
+t = 0   # Defining variable for counting total time
 
 # Defining loop for catching frames
 while True:
-    # Capturing frame-by-frame
-    ret, frame = video.read()
-
-    # If the frame was not retrieved
-    # e.g.: at the end of the video,
-    # then we break the loop
+    # Retrieved and frame from the video
+    ret, frame = video.read()   # Reading frames
+    
     if not ret:
-        break
-
-    # Getting spatial dimensions of the frame
-    # we do it only once from the very beginning
-    # all other frames have the same dimension
+        break #
+    
     if w is None or h is None:
         # Slicing from tuple only first two elements
         h, w = frame.shape[:2]
-
-    """
-    Start of:
-    Getting blob from current frame
-    """
-
-    # Getting blob from current frame
-    # The 'cv2.dnn.blobFromImage' function returns 4-dimensional blob from current
-    # frame after mean subtraction, normalizing, and RB channels swapping
-    # Resulted shape has number of frames, number of channels, width and height
-    # E.G.:
-    # blob = cv2.dnn.blobFromImage(image, scalefactor=1.0, size, mean, swapRB=True)
-    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
-                                 swapRB=True, crop=False)
-
-    """
-    End of:
-    Getting blob from current frame
-    """
-
-    """
-    Start of:
-    Implementing Forward pass
-    """
-
-    # Implementing forward pass with our blob and only through output layers
-    # Calculating at the same time, needed time for forward pass
-    network.setInput(blob)  # setting blob as input to the network
+        
+    blob = cv2.dnn.blobFromImage(
+        frame,
+        1 / 255.0,
+        (416, 416),
+        swapRB=True,
+        crop=False
+    )
+    
+    network.setInput(blob)  # Implementing forward pass
     start = time.time()
     output_from_network = network.forward(layers_names_output)
     end = time.time()
-
-    # Increasing counters for frames and total time
-    f += 1
-    t += end - start
-
-    # Showing spent time for single current frame
+    
+    f+=1
+    f+= end - start
+    
     print('Frame number {0} took {1:.5f} seconds'.format(f, end - start))
-
-    """
-    End of:
-    Implementing Forward pass
-    """
-
-    """
-    Start of:
-    Getting bounding boxes
-    """
-
+    
+    
     # Preparing lists for detected bounding boxes,
     # obtained confidences and class's number
-    bounding_boxes = []
-    confidences = []
-    class_numbers = []
-
+    bounding_boxes  = []
+    confidences     = []
+    class_numbers   = []
+    
     # Going through all output layers after feed forward pass
     for result in output_from_network:
         # Going through all detections from current output layer
@@ -221,21 +97,19 @@ while True:
             # Getting value of probability for defined class
             confidence_current = scores[class_current]
 
-            # # Check point
-            # # Every 'detected_objects' numpy array has first 4 numbers with
-            # # bounding box coordinates and rest 80 with probabilities
-            #  # for every class
-            # print(detected_objects.shape)  # (85,)
+            # Check point
+            # Every 'detected_objects' numpy array has first 4 numbers with
+            # bounding box coordinates and rest 80 with probabilities for every class
 
             # Eliminating weak predictions with minimum probability
             if confidence_current > probability_minimum:
-                # Scaling bounding box coordinates to the initial frame size
+                # Scaling bounding box coordinates to the initial image size
                 # YOLO data format keeps coordinates for center of bounding box
                 # and its current width and height
                 # That is why we can just multiply them elementwise
                 # to the width and height
-                # of the original frame and in this way get coordinates for center
-                # of bounding box, its width and height for original frame
+                # of the original image and in this way get coordinates for center
+                # of bounding box, its width and height for original image
                 box_current = detected_objects[0:4] * np.array([w, h, w, h])
 
                 # Now, from YOLO data format, we can get top left corner coordinates
@@ -245,22 +119,11 @@ while True:
                 y_min = int(y_center - (box_height / 2))
 
                 # Adding results into prepared lists
-                bounding_boxes.append([x_min, y_min,
-                                       int(box_width), int(box_height)])
+                bounding_boxes.append([x_min, y_min, int(box_width), int(box_height)])
                 confidences.append(float(confidence_current))
                 class_numbers.append(class_current)
-
-    """
-    End of:
-    Getting bounding boxes
-    """
-
-    """
-    Start of:
-    Non-maximum suppression
-    """
-
-    # Implementing non-maximum suppression of given bounding boxes
+             
+            # Implementing non-maximum suppression of given bounding boxes
     # With this technique we exclude some of bounding boxes if their
     # corresponding confidences are low or there is another
     # bounding box for this region with higher confidence
@@ -340,16 +203,6 @@ while True:
 
     # Write processed current frame to the file
     writer.write(frame)
-
-    """
-    End of:
-    Writing processed frame into the file
-    """
-
-"""
-End of:
-Reading frames in the loop
-"""
 
 
 # Printing final results
